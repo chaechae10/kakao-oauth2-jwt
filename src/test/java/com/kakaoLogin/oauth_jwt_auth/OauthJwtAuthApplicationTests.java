71e0d9935787ee3f4bfba336d1ac69fae78a86e9
CHAEYEON KIM <chayeon0303@naver.com>
2026-06-11 20:29:18 +0900

    프로젝트 초기 설정

---
+++ src/test/java/com/kakaoLogin/oauth_jwt_auth/OauthJwtAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakaoLogin.oauth_jwt_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthJwtAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
